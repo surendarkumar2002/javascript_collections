2aea5a378d0e2e1c6247ff9bf1d2886ea5170423
surendarkumar2002 <surendarkumar1719@gmail.com>
2024-04-07 12:30:14 +0530

    add new file

---
+++ 03-Quiz-app/app.js
@@ -0,0 +1,44 @@
+const questions = [
+  {
+    question: "which is largest animal in the world?",
+    answers: [
+      {
+        text: "shark",
+        correct: false,
+      },
+      {
+        text: "blue whale",
+        correct: true,
+      },
+      {
+        text: "elephant",
+        correct: false,
+      },
+      {
+        text: "giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "which is samllest continent in the world ?",
+    answers: [
+      {
+        text: "Asia",
+        correct: false,
+      },
+      {
+        text: "Austraila",
+        correct: true,
+      },
+      {
+        text: "Aritc",
+        correct: false,
+      },
+      {
+        text: "Africa",
+        correct: false,
+      },
+    ],
+  },
+];
